c99078c433734566aa60604bdad4e74597baf002
Daniel Shapiro <daniel@lemay.ai>
2020-06-24 12:28:21 -0400

    correcting bug

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as rqf:
 
 setuptools.setup(
     name="gravityai",
-    version="0.0.2",
+    version="0.0.3",
     author="Jon Huss",
     author_email="jon@gravity-ai.com",
     description="The gravity-ai.com helper package",
